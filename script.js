019fa8da10ecf09c956f0a29c6710b2a52dc8dbe
MyhailoSkrypkin <160260158+MyhailoSkrypkin@users.noreply.github.com>
2024-02-23 16:33:15 +0000

    Додали малювання кола по клікові мишкою

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function draw(event) {
     let canvas = document.getElementById("imgCanvas");
+    let ctx = canvas.getContext("2d");
+    let rect = canvas.getBoundingClientRect();
+
+    let posX = event.clientX;
+    let posY = event.clientY;
+
+    ctx.fillStyle = "#eeaaaa"
+
+    ctx.arc(posX, posY, 50, 0, 2 * Math.PI)
 }
